26366efd330e3b27a343bda1e5e036b7d0e48038
Rafael-Ernesto-Perez <rafaelperezctes@gmail.com>
2019-07-16 18:26:54 -0300

    Update p1-msa.py
    Se agrego la descripción de un problema

--- desafio-1/p1-msa.py
+++ desafio-1/p1-msa.py
@@ -4,6 +4,23 @@ Created on 01/03/2017
 
 @author: Perez Ernesto Rafael
 """
+
+"""
+Problema ejemplo:
+Considerando la siguiente función python:
+def my_func(r, n):
+for i in xrange(n): r = hashlib.md5(r[:9]).hexdigest()
+return r
+calcular el valor de:
+my_func("00000000000000000000000000000000", 2017201720172017)
+Algunos ejemplos concretos de la función:
+my_func("00000000000000000000000000000000", 0) = 4c93008615c2d041e33ebac605d14b5b
+my_func("00000000000000000000000000000000", 1) = c6246d2a39695fb74971b09ced30874f
+my_func("00000000000000000000000000000000", 2) = 5e1c33543fbcd9dad6a3f8c07887b8d2
+my_func("c6246d2a39695fb74971b09ced30874f", 0) = 5e1c33543fbcd9dad6a3f8c07887b8d2
+my_func("00000000000000000000000000000000", 2017) = bc107c24b44b5a518b4286ed7e5a1bb3
+
+"""
 import hashlib
 from time import time
 
